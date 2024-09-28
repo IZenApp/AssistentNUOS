65ef9228ee2430b5fd7ea6deaa1f54ea7fcebc62
Yevhenii Bachula <evgene352@gmail.com>
2024-09-28 21:14:56 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# StudentAssistantApp.web
+# StudentAssistant NUOS (nuos.edu.ua)
+
+## About the App
+
+**Student Assistant App** is designed to simplify and enhance the student experience. This web application serves as a one-stop platform where students can access various resources, stay updated with events, and improve their academic journey.
+
+## Technologies Used
+
+- **HTML5/CSS3**: For structuring and styling the user interface.
+- **JavaScript**: To handle interactivity and user actions.
+- **PWA (Progressive Web App)**: Add the app to mobile devices for quick access.
+
+## Join Us
+
+Feel free to fork this project and contribute to further enhancements. We welcome your suggestions!
+
---
+++ service-worker.js
@@ -0,0 +1,92 @@
+self.addEventListener('install', (event) => {
+    const htmlFiles = [
+        './index.html',
+        './html/About Home.html',
+        './html/About the University.html',
+        './html/Call schedule.html',
+        './html/Contacts Dean of faculties.html',
+        './html/Leadership of the student republic.html',
+        './html/Manual.html',
+        './html/Structure of the University.html',
+        './html/Student republic.html',
+        './html/Student.html'
+    ];
+
+    const cssFiles = [
+        './css/global/global1.css',
+        './css/global/global2.css',
+        './css/global/global3.css',
+        './css/global/global4.css',
+        './css/global/global5.css',
+        './css/global/global6.css',
+        './css/global/global7.css',
+        './css/global/global8.css',
+        './css/global/global9.css',
+        './css/global/global10.css',
+        './css/index1.css',
+        './css/gl.css',
+        './css/About Home.css',
+        './css/About the University.css',
+        './css/Call schedule.css',
+        './css/Leadership of the student republic.css',
+        './css/Structure of the University.css',
+        './css/Student republic.css',
+        './css/Student.css',
+        './css/Contacts Dean of faculties.css',
+        './css/Manual.css',
+        './process/css/Leadership of the student republic.css'
+    ];
+
+    const imageFiles = [
+        './img/1.png',
+        './img/2.png',
+        './img/3.png',
+        './img/5.png',
+        './img/4.png',
+        './img/6.png',
+        './img/7.png',
+        './img/8.png',
+        './img/9.png',
+        './img/Fon1.png',
+        './img/Fon2.png',
+        './img/Fon3.png',
+        './img/Fon4.png',
+        './img/Fon5.png',
+        './img/Fon6.png',
+        './img/Fon7.png',
+        './img/Fon8.png',
+        './img/Fon9.png',
+        './img/Fon10.png',
+        './img/LogoMain.png',
+        './img/Logo1.png',
+        './img/Logo2.png',
+        './img/Logo.png',
+        './img/Avatar/Illia.png',
+        './img/Avatar/Katya.png'
+    ];
+
+    const allFiles = [...htmlFiles, ...cssFiles, ...imageFiles, './index.html'];
+
+    event.waitUntil(
+        caches.open('assistant-cache').then((cache) => {
+            return cache.addAll(allFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+  
