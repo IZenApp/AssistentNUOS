1b45bc1303e3a9be0b0bd7d44985dbfb148baf41
Yevhenii Bachula <evgene352@gmail.com>
2024-10-06 20:19:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = 'assistant-cache-v2'; // Новая версия кеша
+
 self.addEventListener('install', (event) => {
     const htmlFiles = [
         './index.html',
@@ -68,16 +70,23 @@ self.addEventListener('install', (event) => {
     const allFiles = [...htmlFiles, ...cssFiles, ...imageFiles, './index.html'];
 
     event.waitUntil(
-        caches.open('assistant-cache').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(allFiles);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
@@ -89,4 +98,3 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
-  
