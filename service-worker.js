ef234d813300860778aa7d5be201ba79e12084ee
All0N <evgene352@gmail.com>
2024-10-15 14:56:17 +0300

    modified:   html/About Home.html 	modified:   index.html 	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Имя кеша. Увеличивай номер версии при каждом обновлении файлов.
-const CACHE_NAME = 'assistant-cache-v4.0'; 
+const CACHE_NAME = 'assistant-cache-v4.1'; 
 
 // Обработчик события установки service worker
 self.addEventListener('install', (event) => {
