78a610d87937cc45e92e6a052f146e6c3eb9211d
Yevhenii Bachula <evgene352@gmail.com>
2024-11-01 01:17:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Имя кеша. Увеличивай номер версии при каждом обновлении файлов.
-const CACHE_NAME = 'assistant-cache-v5.1'; 
+const CACHE_NAME = 'assistant-cache-v5.2'; 
 
 // Обработчик события установки service worker
 self.addEventListener('install', (event) => {
