c4309ba7391e37ea133e1148ac84b79f0ca8865b
Yevhenii Bachula <evgene352@gmail.com>
2025-08-14 03:02:02 +0300

    modified:   manifest.json 	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // ===== SERVICE WORKER ДЛЯ АСИСТЕНТА НУОС =====
 
-const CACHE_NAME = 'nuos-assistant-v4.0';
+const CACHE_NAME = 'nuos-assistant-v4.1';
 const OFFLINE_URL = '/pages/offline.html';
 
 // Ресурси для кешування
